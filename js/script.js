7f02ac73ff9285c03ebb04c21af497db98c13f12
arjunrawal4 <20448923+arjunrawal4@users.noreply.github.com>
2019-01-23 23:03:30 -0600

    routing updates

--- js/script.js
+++ js/script.js
@@ -2,7 +2,18 @@ $(function(){
 	var container = $("#container")
 	container.shuffleLetters();
 	setTimeout(function(){
+	},4000);
+
+	var container1 = $("#container1")
+	container1.shuffleLetters();
+	setTimeout(function(){
+	},6000);
+	
+	var container2 = $("#container2")
+	container2.shuffleLetters();
+	setTimeout(function(){
 	},8000);
+
 });
 
 $(document).ready(function () {
